4a56aaad976ea014cfaf258867c93204ad7acc5a
cjettie <ferundal@gmail.com>
2021-10-29 22:17:19 +0300

    ex 01 started

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,62 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat( void ) {
+
+}
+
+Bureaucrat::Bureaucrat( const std::string new_name, int new_grade) : name(new_name) {
+	if (new_grade > GRADE_MAX_INT_VAL) {
+		throw GradeTooLowException();
+	}
+	if (new_grade < GRADE_MIN_INT_VAL) {
+		throw GradeTooHighException();
+	}
+	grade = new_grade;
+}
+Bureaucrat::Bureaucrat( const Bureaucrat& origin) : name (origin.name), grade(origin.grade) {
+
+}
+
+Bureaucrat& Bureaucrat::operator =(const Bureaucrat& origin) {
+	grade = origin.grade;
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat( void ) {
+
+}
+
+Bureaucrat Bureaucrat::operator ++(int) {
+	Bureaucrat ret_val (*this);
+	if (grade == GRADE_MIN_INT_VAL) {
+		throw GradeTooHighException();
+	}
+	--grade;
+	return (ret_val);
+}
+Bureaucrat Bureaucrat::operator --(int) {
+	Bureaucrat ret_val (*this);
+	if (grade == GRADE_MAX_INT_VAL) {
+		throw GradeTooLowException();
+	}
+	return (ret_val);
+}
+std::string Bureaucrat::getName( void ) const {
+	return name;
+}
+int Bureaucrat::getGrade ( void ) const {
+	return grade;
+}
+
+const char * Bureaucrat::GradeTooHighException::what() const throw() {
+	return ("Grade too high!");
+}
+
+const char * Bureaucrat::GradeTooLowException::what() const throw() {
+	return ("Grade too low!");
+}
+
+std::ostream &operator <<(std::ostream &outstream, const Bureaucrat &origin) {
+	outstream << origin.getName() << ", bureaucrat grade " << origin.getGrade() << "!";
+	return (outstream);
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,54 @@
+#include "Bureaucrat.hpp"
+#define ANIMAL_ARRAY_SIZE 2
+
+int main() {
+	try
+	{
+		Bureaucrat ok_bur ("Ok_Bur", 67);
+		std::cout << ok_bur <<std::endl;
+		ok_bur++;
+		std::cout << ok_bur <<std::endl;
+		ok_bur--;
+		std::cout << ok_bur <<std::endl;
+	}
+	catch (std::exception & e)
+	{
+		std::cout << "You shoud not see that message!" <<std::endl;
+	}
+	try
+	{
+		Bureaucrat ok_bur ("Ok_Bur", 1);
+		std::cout << ok_bur <<std::endl;
+		ok_bur++;
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat ok_bur ("Ok_Bur", 150);
+		std::cout << ok_bur <<std::endl;
+		ok_bur--;
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat not_ok_bur ("Not_ok_Bur", -7);
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat not_ok_bur ("Not_ok_Bur", -7);
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
